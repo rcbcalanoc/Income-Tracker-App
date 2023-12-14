d706fae86bb0e6b992fd4c80a245b8e6fedd74f3
Richard Carlos Calanoc <richardbcalanoc@gmail.com>
2023-12-14 17:57:36 +0800

    Initial Commit, Design Completed

---
+++ app/src/main/java/com/example/calanoc_ex3/FirstFragment.java
@@ -0,0 +1,8 @@
+package com.example.calanoc_ex3;
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+}
---
+++ app/src/main/java/com/example/calanoc_ex3/SecondFragment.java
@@ -0,0 +1,9 @@
+package com.example.calanoc_ex3;
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+}
